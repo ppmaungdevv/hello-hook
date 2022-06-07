0ded34bea14ef946598c32e6c5a9e2f2ddb36d51
ppmaungdev <ppmaungdev@gmail.com>
2022-06-07 21:02:44 +0630

    effect hook conditional run effect

--- src/Components/EffectHookCounter.js
+++ src/Components/EffectHookCounter.js
@@ -2,11 +2,19 @@ import React, {useState, useEffect} from 'react'
 
 function EffectHookCounter() {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
+    // useEffect(() => {
+    //     console.log('title change')
+    //     document.title = `Clicked ${count}`
+    // })
     useEffect(() => {
+        console.log('title change')
         document.title = `Clicked ${count}`
-    })
+    }, [count]) 
+    // in [], [count], add props or states that need to be watched and only these states or props has been  changed the effect will execute
   return (
     <div>
+        <input type='text' value={name} onChange={e => setName(e.target.value)} />
         <button onClick={() => setCount(count + 1)}>Clicked Count {count}</button>
     </div>
   )
