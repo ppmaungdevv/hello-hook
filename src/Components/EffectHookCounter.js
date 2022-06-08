d984e9d50a10c34233c1c0ca0ae2a321df119812
ppmaungdev <ppmaungdev@gmail.com>
2022-06-08 20:24:30 +0630

    effect hook run once

--- src/Components/EffectHookCounter.js
+++ src/Components/EffectHookCounter.js
@@ -1,21 +1,40 @@
 import React, {useState, useEffect} from 'react'
 
 function EffectHookCounter() {
-    const [count, setCount] = useState(0)
-    const [name, setName] = useState('')
+    // const [count, setCount] = useState(0)
+    // const [name, setName] = useState('')
     // useEffect(() => {
     //     console.log('title change')
     //     document.title = `Clicked ${count}`
     // })
-    useEffect(() => {
-        console.log('title change')
-        document.title = `Clicked ${count}`
-    }, [count]) 
+
+    // ------ conditionally run effect
+    // useEffect(() => {
+    //     console.log('title change')
+    //     document.title = `Clicked ${count}`
+    // }, [count]) 
     // in [], [count], add props or states that need to be watched and only these states or props has been  changed the effect will execute
+
+    // ------ run effect Only Once
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+
+    const logMousePosition = e => {
+      console.log('mouse')
+      setX(e.clientX)
+      setY(e.clientY)
+    }
+
+    useEffect(() => {
+      console.log('effect called')
+      window.addEventListener('mousemove', logMousePosition)
+    }, []) // called effect only once like componentDidMount in class
+
   return (
     <div>
-        <input type='text' value={name} onChange={e => setName(e.target.value)} />
-        <button onClick={() => setCount(count + 1)}>Clicked Count {count}</button>
+        {/* <input type='text' value={name} onChange={e => setName(e.target.value)} />
+        <button onClick={() => setCount(count + 1)}>Clicked Count {count}</button> */}
+        X coordinate {x} Y coordinate {y}
     </div>
   )
 }
