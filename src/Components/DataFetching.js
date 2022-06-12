3270e80138e9ce181ea6dbb945d18de5dca62289
ppmaungdev <ppmaungdev@gmail.com>
2022-06-12 20:15:32 +0630

    effectHook with data fetching part 3 with button clicked

--- src/Components/DataFetching.js
+++ src/Components/DataFetching.js
@@ -7,18 +7,24 @@ function DataFetching() {
     // const [posts, setPosts] = useState([])
     const [post, setPost] = useState({})
     const [post_id, setPostId] = useState(1)
+    const [post_id_from_btn, setPostIdFromBtn] = useState(1)
+
+    const clickedFetch = () => {
+        setPostIdFromBtn(post_id)
+    }
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts/'+ post_id).then(({data}) => {
+        axios.get('https://jsonplaceholder.typicode.com/posts/'+ post_id_from_btn).then(({data}) => {
             setPost(data)
             console.log(data)
         }).catch(err => {
             console.log(err)
         })
-    }, [post_id])
+    }, [post_id_from_btn])
   return (
     <div>
         <input type='text' value={post_id} onChange={e => setPostId(e.target.value)} />
+        <button onClick={clickedFetch}>Fectch</button>
         <p>
             {post.title}
         </p>
